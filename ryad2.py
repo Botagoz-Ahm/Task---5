637ed98af83bae4cddd1f03a0d3628978ec9e1ad
Botagoz-Ahm <62739782+Botagoz-Ahm@users.noreply.github.com>
2020-05-02 04:44:00 +0600

    Update and rename ряд2.py to ryad2.py

--- ryad2.py
+++ ryad2.py
@@ -1,10 +1,10 @@
 A = int(input())
 B = int(input())
 if A < B:
-    for myRange in range(A, B+1):
+    for myRange in range(A, B + 1):
         print(myRange, end=' ')
 elif A > B:
-    for myRange in range(A, B-1, -1):
+    for myRange in range(A, B - 1, -1):
         print(myRange, end=' ')
 else:
     print(A or B)
